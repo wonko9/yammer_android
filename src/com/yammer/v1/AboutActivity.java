4b028a6d60c8453e938f62dbe56f0d6ab5ad3b20
Scott Steadman <ss@stdmn.com>
2010-01-16 10:56:18 -0800

    Cleaned up AboutActivity.

--- src/com/yammer/v1/AboutActivity.java
+++ src/com/yammer/v1/AboutActivity.java
@@ -1,30 +1,27 @@
 package com.yammer.v1;
 
 import android.app.Activity;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
+  
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.about);
-		// Get version info
-		PackageManager pm = getPackageManager();
-		PackageInfo pi;
-		String versionName = "";
-		try {
-			pi = pm.getPackageInfo(getPackageName(), 0);
-			versionName = pi.versionName;
-		} catch (NameNotFoundException e) {
-			e.printStackTrace();
-			versionName = "unknown";
-		}
 		TextView textView = (TextView)findViewById(R.id.about_text_view);
-		textView.setText(String.format(getResources().getString(R.string.about_text), versionName));
+		textView.setText(String.format(getResources().getString(R.string.about_text), getVersionName()));
+	}
+	
+	private String getVersionName() {
+    try {
+      return getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+    } catch (NameNotFoundException e) {
+      e.printStackTrace();
+    }
+    return "unknown";
 	}
 }
